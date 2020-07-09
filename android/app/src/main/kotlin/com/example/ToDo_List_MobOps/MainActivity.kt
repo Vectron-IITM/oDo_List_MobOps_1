1aaf3461d9ac7fdd43950c45d6dff8c819b56a8c
Vectron-IITM <65945306+Vectron-IITM@users.noreply.github.com>
2020-07-10 05:03:18 +0530

    FirstEdit

---
+++ android/app/src/main/kotlin/com/example/ToDo_List_MobOps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ToDo_List_MobOps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import './todo.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  TodoListState createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  List<Todo> todos = [];
+  TextEditingController controller = new TextEditingController();
+
+  toggleTodo(Todo todo, bool isChecked) {
+    setState(() {
+      todo.isDone = isChecked;
+    });
+  }
+
+  Widget buildItems(BuildContext context, int index) {
+    final todo = todos[index];
+
+    return CheckboxListTile(
+      value: todo.isDone,
+      title: Text(todo.title),
+      onChanged: (bool isChecked) {
+        toggleTodo(todo, isChecked);
+      },
+    );
+  }
+
+  addTodo() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Today\'s Tasks'),
+            content: TextField(
+              controller: controller,
+              autofocus: true,
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    final todo = new Todo(title: controller.value.text);
+                    todos.add(todo);
+                    controller.clear();
+                    Navigator.of(context).pop();
+                  });
+                },
+                child: Text('Add'),
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Today'),
+        backgroundColor: Colors.red[400],
+      ),
+      body: empty(todos.length),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+        ),
+        backgroundColor: Colors.red[400],
+        onPressed: addTodo,
+      ),
+    );
+  }
+
+  void removeItemFromList(item) {
+    setState(() {
+      deleteItem(item);
+    });
+  }
+
+  void deleteItem(item) {
+    todos.remove(item);
+  }
+
+  Widget empty(items) {
+    if (items > 0) {
+      return ListView.builder(
+        itemBuilder: buildItems,
+        itemCount: todos.length,
+      );
+    } else {
+      return Center(
+          child: Text(
+        'You\'re all done for today! \n #TodoListZero',
+        style: TextStyle(fontSize: 20),
+      ));
+    }
+  }
+}
